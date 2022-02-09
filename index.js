c64d303462d05211aa20a951e1deb85655a1d0e1
Victoria-V-V <victoriavolcheck@yandex.ru>
2022-02-09 13:02:29 +0700

    Added classList instead of style in js

--- index.js
+++ index.js
@@ -2,18 +2,27 @@
 function showTheme() {
     switch (document.getElementById("theme").value) {
         case "light":
-            document.body.style.backgroundColor = 'white';
-            document.body.style.color = 'black';
+            // document.body.style.backgroundColor = 'white';
+            // document.body.style.color = 'black';
+            document.body.classList.remove('blueStyle');
+            document.body.classList.remove('darkStyle');
+            document.body.classList.add('lightStyle');
             break;
 
         case "blue":
-            document.body.style.backgroundColor = 'lightblue';
-            document.body.style.color = 'blue';
+            // document.body.style.backgroundColor = 'lightblue';
+            // document.body.style.color = 'blue';
+            document.body.classList.remove('lightStyle');
+            document.body.classList.remove('darkStyle');
+            document.body.classList.add('blueStyle');
             break;
 
         case "dark":
-            document.body.style.backgroundColor = 'rgb(43, 43, 43)';
-            document.body.style.color = 'white';
+            // document.body.style.backgroundColor = 'rgb(43, 43, 43)';
+            // document.body.style.color = 'white';
+            document.body.classList.remove('lightStyle');
+            document.body.classList.remove('blueStyle');
+            document.body.classList.add('darkStyle');
             break;
     }
 }
@@ -22,8 +31,8 @@ function showTheme() {
 function operation() {
     let number1 = Number(document.getElementById('number1').value);
     let number2 = Number(document.getElementById('number2').value);
-    
-    switch(operator) {
+
+    switch (operator) {
         case '+':
             result = number1 + number2;
             break;
@@ -38,18 +47,13 @@ function operation() {
 
         case '/':
             if (!number2) {
-            result = ("На ноль делить нельзя!");
-            break;
-            }
-            else {
-            result = number1 / number2;
-            break;
+                result = ("На ноль делить нельзя!");
+                break;
+            } else {
+                result = number1 / number2;
+                break;
             }
     }
 
-        document.getElementById('result').innerHTML = result;
+    document.getElementById('result').innerHTML = result;
 }
-
-
-
-
